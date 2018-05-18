4c6a35bae69257a4ddd7c276f4b436604b54b8c7
Георгий Бухаров <gbu@crtweb.ru>
2018-05-18 18:05:00 +0500

    Add: project structure

---
+++ project/src/components/Developers/Developers.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li
+      v-for="(developer, index) in developers"
+      :key="index">
+      <div>Name: {{ developer.name }}</div>
+      <div>Framework: {{ developer.framework }}</div>
+      <div>Description: {{ developer.description }}</div>
+      <div>Salary: {{ developer.salary }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { events } from "../../main.js";
+export default {
+  data() {
+    return {
+      developers: [],
+    }
+  },
+  methods: {
+  },
+  created() {
+    fetch('https://developers-c2f70.firebaseio.com/developers.json')
+      .then((response) => response.json())
+      .then((data) => {
+        this.developers = data;
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
